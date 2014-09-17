395b3523b2386c289e6bed2db65bf24dcfb7d615
Alex <alex.saenbandith@drake.edu>
2014-09-17 16:58:45 -0500

    added picture files

---
+++ gen/edu/drake/desmoinessocialclubcalendar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.drake.desmoinessocialclubcalendar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
