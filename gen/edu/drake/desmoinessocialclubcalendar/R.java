632b780a0dcfad6b6f60c754de8ead4c3855d5a4
zijiansaw <zijian.saw@drake.edu>
2014-09-18 14:14:17 -0500

    icon change

--- gen/edu/drake/desmoinessocialclubcalendar/R.java
+++ gen/edu/drake/desmoinessocialclubcalendar/R.java
@@ -27,10 +27,11 @@ public final class R {
         public static final int dmsc_mockup_5_on=0x7f020002;
         public static final int event=0x7f020003;
         public static final int ic_launcher=0x7f020004;
-        public static final int landing_a=0x7f020005;
-        public static final int landing_b=0x7f020006;
-        public static final int search_by_d=0x7f020007;
-        public static final int search_by_ev=0x7f020008;
+        public static final int icon=0x7f020005;
+        public static final int landing_a=0x7f020006;
+        public static final int landing_b=0x7f020007;
+        public static final int search_by_d=0x7f020008;
+        public static final int search_by_ev=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
